d5828a6b9a952628a9061b9ca81d2f3fb0ca3fe0
Riter Cordova <RiterCordova@Mac-mini.local>
2018-04-12 18:20:56 -0400

    code is not prime return string FAIL for response

--- urbanager-application/src/main/java/com/alenasoft/urbanager/resources/numericalperation/NumericalOperationsServiceImpl.java
+++ urbanager-application/src/main/java/com/alenasoft/urbanager/resources/numericalperation/NumericalOperationsServiceImpl.java
@@ -24,7 +24,7 @@ public class NumericalOperationsServiceImpl implements NumericalOperationsServic
     public String isPrime(int number) {
         if (evalPrimo(number))
             return IS_PRIME;
-
-        return null;
+        else
+            return FAIL_PRIME;
     }
 }
