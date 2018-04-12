d194e5ffbd60dd9931dc806e6fc648686a73bcf5
Riter Cordova <RiterCordova@Mac-mini.local>
2018-04-12 18:23:29 -0400

    code resource url primo/entero for validate if number is prime

--- urbanager-application/src/main/java/com/alenasoft/urbanager/resources/numericalperation/NumericalOperationsResource.java
+++ urbanager-application/src/main/java/com/alenasoft/urbanager/resources/numericalperation/NumericalOperationsResource.java
@@ -6,10 +6,18 @@ import javax.ws.rs.*;
 @Path("primo")
 public class NumericalOperationsResource {
 
+    @Inject
+    private NumericalOperationsService service;
+
     @GET
     public String pin() {
         return "/primo";
     }
 
+    @GET
+    @Path("{entero}")
+    public String getResult(@PathParam("entero") int entero) {
+        return this.service.isPrime(entero);
+    }
 }
 
